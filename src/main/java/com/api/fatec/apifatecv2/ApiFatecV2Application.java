7f7d8c3c3e2d6e9104136d5d7c91ef9d8c421d71
TeraJolt <jollimas14@outlook.com>
2024-05-10 00:05:01 -0300

    Adicionado projeto Maven

---
+++ src/main/java/com/api/fatec/apifatecv2/ApiFatecV2Application.java
@@ -0,0 +1,13 @@
+package com.api.fatec.apifatecv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFatecV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFatecV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/fatec/apifatecv2/ApiFatecV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.fatec.apifatecv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFatecV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
